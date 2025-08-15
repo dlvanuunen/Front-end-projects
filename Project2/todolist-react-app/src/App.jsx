6284c854e630b80bed4fd2be071225a32dc3125e
Voortuinkabouter <129389575+Voortuinkabouter@users.noreply.github.com>
2025-08-15 21:08:45 +0200

    created Project2

---
+++ Project2/todolist-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+ 
+          Edit <code>src/App.jsx</code> and save to test HMR
+
+   
+    </>
+  )
+}
+
+export default App
