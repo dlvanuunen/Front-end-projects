981adf8a38b58e10731f309328da036274dcf39c
Voortuinkabouter <129389575+Voortuinkabouter@users.noreply.github.com>
2025-08-23 00:02:19 +0200

    Redesigned card styles

--- Project3/anime-react-app/src/components/AnimeCard.jsx
+++ Project3/anime-react-app/src/components/AnimeCard.jsx
@@ -1,23 +1,34 @@
-import '../css/anime-card2.css'
+import "../css/anime-card.css";
+import { useEffect, useRef } from "react";
 
 function AnimeCard({ title, year, img }) {
   return (
     <>
-    <div className="anime-card" >
-      <div className="card-poster">
-     
-      <img src={img}/>
+      <div className="anime-card">
+                      
+        <div className="card-poster">
+          <img src={img} />
+          <div className="gradient-effect"></div>
+        </div>
+
+
+        <div className="card-label">
+          <div className="card-header">
+            <h3>{title}</h3>
+
+            <p>{year}</p>
+          </div>
+
+          <div className="card-footer">
+            <div className="card-tags">
+              <span>Mystery</span>
+              <span>Action</span>
+            </div>
+            <button>{"♥"} </button>
+          </div>
+        </div>
+
       </div>
-    
-    <div className="card-label">
-           <h3>{title}</h3>
-      <p>{year}</p>
-    <div className="card-tags"></div>
-    <button>Favorite</button>
-</div>
-
-    </div>
-
     </>
   );
 }
