e66094f8dffe7eb4d75bb58f0e8d7ec870bb38dd
Voortuinkabouter <129389575+Voortuinkabouter@users.noreply.github.com>
2025-08-26 20:17:33 +0200

    completed story section (img still placeholder), adjusted navbar fontsize and button

--- Project4/Tideline-landing-page/src/Components/StorySection.jsx
+++ Project4/Tideline-landing-page/src/Components/StorySection.jsx
@@ -1,35 +1,34 @@
-import CtaButton from '../Parts/CtaButton';
+import CtaButton from "../Parts/CtaButton";
 
+function StorySection() {
+  return (
+    <>
+      <section className="story">
+        <h2>The Story</h2>
 
+        <p>
+          Every shard reborn carries the story of a tide. Our artisans transform
+          discarded ocean glass into functional works of art, honoring both the
+          sea's beauty and our commitment to sustainability.
+        </p>
+        <p>
+          Each piece begins its journey in the ocean's embrace, weathered by
+          salt and waves until it reaches our workshop. There, skilled hands
+          shape it into dinnerware that brings the ocean's serenity to your
+          table.
+        </p>
 
-function StorySection(){
+        <p>
+          The imperfections tell the story—no two pieces are identical, just as
+          no two waves are the same. This is luxury with purpose, beauty with
+          meaning.
+        </p>
 
-
-return<>
-
-<section className="story">
-
-<h2>The Story</h2>
-
-<p>
-
-Every shard reborn carries the story of a tide. Our artisans transform discarded ocean glass into functional works of art, honoring both the sea's beauty and our commitment to sustainability.
-
-Each piece begins its journey in the ocean's embrace, weathered by salt and waves until it reaches our workshop. There, skilled hands shape it into dinnerware that brings the ocean's serenity to your table.
-
-The imperfections tell the story—no two pieces are identical, just as no two waves are the same. This is luxury with purpose, beauty with meaning.
-
-
-</p>
-
-<CtaButton text="Discover our story" type="alt"/>
-<img src="/images/raw-glass.jpg"></img>
-
-</section>
-</>
-
-
-
+        <CtaButton text="Discover Our Story" type="alt" />
+        <img src="/images/raw-glass.jpg"></img>
+      </section>
+    </>
+  );
 }
 
-export default StorySection
+export default StorySection;
