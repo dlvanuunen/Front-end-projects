42c938d5c426a62b15dab6213fd44138d57f509f
Voortuinkabouter <129389575+Voortuinkabouter@users.noreply.github.com>
2025-09-03 00:18:39 +0200

    Basic layout of the dashboard, svg area for graph. next step: add axis and display mock data

--- Project5/dashboard-react-app/src/App.jsx
+++ Project5/dashboard-react-app/src/App.jsx
@@ -1,35 +1,94 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import ChartArea from './Components/ChartArea';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dark, setDark] = useState(false);
+  const [tab, setTab] = useState("overview");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div data-theme={dark ? "dark" : "light" } >
+
+     <div className="wrapper">
+      <header>
+      <nav>
+        <div className="navbar">
+        <div className="navbar-logo text-heading">🌿 Air Quality</div>
+        <div className="navbar-tabs">
+          <a
+            href="#"
+            className={tab === "overview" ? "active" : ""}
+            onClick={() => setTab("overview")}
+          >
+            Overview
+          </a>
+          <a
+            href="#"
+            className={tab === "details" ? "active" : ""}
+            onClick={() => setTab("details")}
+          >
+            Details
+          </a>
+        </div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={dark}
+            onChange={() => setDark(!dark)}
+          />
+          <span className="slider"></span>
+        </label>
+        </div>
+      </nav>
+      </header>
+
+      {/* Dashboard Container */}
+      <main className="container">
+        {/* KPI Cards */}
+        <section className="grid grid-3">
+          <div className="card">
+            <h3>PM2.5</h3>
+            <p className="value text-kpi">42 µg/m³</p>
+            <p className="label text-label">Moderate</p>
+          </div>
+          <div className="card">
+            <h3>CO₂</h3>
+            <p className="value text-kpi">720 ppm</p>
+            <p className="label text-label">Acceptable</p>
+          </div>
+          <div className="card">
+            <h3>O₃</h3>
+            <p className="value text-kpi">90 µg/m³</p>
+            <p className="label text-label">Elevated</p>
+          </div>
+        </section>
+
+        {/* Filters */}
+        <section className="filter-group" >
+          <input type="text" placeholder="Search location..." />
+          <select>
+            <option>Last 7 days</option>
+            <option>Last 30 days</option>
+            <option>Last year</option>
+          </select>
+          <button>Apply</button>
+        </section>
+
+        {/* Chart */}
+        <section className="card">
+          <h3>Pollution Trend</h3>
+          <p className="label text-label">D3 chart will go here</p>
+          <ChartArea/>
+
+        </section>
+      </main>
+    </div></div>
+  
+   
+  );
 }
+
 
 export default App
---
+++ Project5/dashboard-react-app/src/Components/ChartArea.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import * as d3 from "d3"
+
+
+function ChartArea(){
+
+
+const data = [{x: 1, y:4}, {x:2, y: 16}, {x:3, y:32}]
+
+
+const area = {
+    margin:{
+    top: 20,
+    right: 20,
+    bottom: 40,
+    left: 40}
+  };
+  const axis = {
+    margin: {
+      xaxis: 13,
+      yaxis: 13,
+      xlabel: 20,
+      ylabel: 20,
+    },
+    tickCount: {
+      x: 10,
+      y: 5,
+    }
+  };
+  const theme = {
+    background_color: "burgundy",
+    chartarea_color: "grey",
+    axis: {
+      color: "black",
+      stroke: "rgba(0, 0, 0, 1)",
+      stroke_width: 1,
+    },
+  };
+
+const config = {area:area, axis:axis, theme:theme}
+
+
+let width = 400
+let height = 400
+
+
+let chartarea_width = (width - area.margin.left - area.margin.right);
+let chartarea_height = (height-area.margin.top-area.margin.bottom);
+
+console.log(chartarea_height)
+
+
+
+    return(
+<>
+
+<p> Display value: {area.margin.top} will be the chart area</p>
+<div className="graph-container" style={{width: "100%", height: "500px"}}>
+<svg className="graph-svg"  >
+<rect
+  width={width}
+  height={height}
+  fill={theme.background_color}
+  />
+ <g transform={`translate(${area.margin.left}, ${area.margin.top})`}>
+  <rect
+  width={chartarea_width}
+  height={chartarea_height}
+  fill={theme.chartarea_color}
+  />   </g>
+
+</svg>
+</div>
+</>
+
+
+    )
+
+}
+
+
+export default ChartArea
